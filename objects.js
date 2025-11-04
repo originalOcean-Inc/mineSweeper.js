e2c20fad684a859192ed2983ddd4d9b62844509b
Scott Adams <scottadamsx@gmail.com>
2025-11-04 17:24:15 -0330

    MAJOR LOGIC CHANGE
    ive added in the functionality that when the game is initialized, the deadzone is created, the bombs are placed, and the markers are set up. if not yet figured out how to have it so that the markers on the edge are revealed, i think i will have logic for the click so that if you click and empty square, It will reveal all the empty cells it touches, and add in a showMarker() function to show the outer ring of the markers

--- objects.js
+++ objects.js
@@ -4,25 +4,37 @@ export class Cell {
         this.y = y
         this.display = display
         this.state = "empty"
+        this.marker = null
     }
     getCoordinates() {
         return `${this.x}${this.y}`
     }
     click(grid) {
+        // LOGIC FOR GRID SETUP
         if (grid.state == "initial") {
             grid.changeState()
-            let surroundingCells = grid.getSurroundingCells(this)
-            grid.createDeadZone(this)
+            // change the values of the clicked cell and create deadzone
             this.state = "dead"
             this.display.style.backgroundColor = "white"
+            grid.createDeadZone(this)
+            
+            // place bombs 
+            grid.placeBombs()
 
+            // identify markers
+            grid.identifyMarkers()
         } 
+
         if (this.state == "empty") {
             this.state = "clicked"
             console.log(this.x,this.y," cell clicked!")
             this.display.style.backgroundColor = "black"
-        } else {
-            console.log("button already clicked!")
+        } else if (this.state == "bomb") {
+            console.log(this.x,this.y,"BOOM!")
+            this.display.style.backgroundColor = "red"
+        } else if (this.state == "marker") {
+            this.display.style.backgroundColor = "white"
+            this.display.textContent = this.marker
         }
     }
 }
@@ -38,6 +50,15 @@ export class Grid {
     // this method works as expected
     addCell(cell) {
         this.cells.push(cell)
+    }
+    getEmptyCells() {
+        let emptyCells = []
+        this.cells.forEach(cell => {
+            if (cell.state == "empty") {
+                emptyCells.push(cell)
+            }
+        })
+        return emptyCells
     }
     // this method works as expected
     getState() {
@@ -81,10 +102,49 @@ export class Grid {
         if (bottomCell != null) {
             surroundingCells.push(bottomCell)
         }
-        console.log(surroundingCells)
         return surroundingCells
     }
+    getAllSurroundingCells(cell) {
+        let surroundingCells = []
+        let x = cell.x
+        let y = cell.y
+        
+        let leftCell = this.getCell(x - 1,y)
+        if (leftCell != null) {
+            surroundingCells.push(leftCell)
+        }
+        let topLeftCell = this.getCell(x - 1,y + 1)
+        if (topLeftCell != null) {
+            surroundingCells.push(topLeftCell)
+        }
+        let topCell = this.getCell(x,y + 1)
+        if (topCell != null) {
+            surroundingCells.push(topCell)
+        }
+        let topRightCell = this.getCell(x + 1,y + 1)
+        if (topRightCell != null) {
+            surroundingCells.push(topRightCell)
+        }
+        let rightCell = this.getCell(x + 1,y)
+        if (rightCell != null) {
+            surroundingCells.push(rightCell)
+        }
+        let bottomRightCell = this.getCell(x + 1,y - 1)
+        if (bottomRightCell != null) {
+            surroundingCells.push(bottomRightCell)
+        }
+        let bottomCell = this.getCell(x,y - 1)
+        if (bottomCell != null) {
+            surroundingCells.push(bottomCell)
+        }
+        let bottomLeftCell = this.getCell(x -1,y - 1)
+        if (bottomLeftCell != null) {
+            surroundingCells.push(bottomLeftCell)
+        }
+        
 
+        return surroundingCells
+    }
     createDeadZone(cell) {
         // we want the deadzone to be a minimum of like 12 cells but a maximum of 40%
         let totalCells = this.width * this.height
@@ -108,4 +168,46 @@ export class Grid {
         currentCell = surroundingCells[Math.floor(Math.random() * surroundingCells.length)]
         }
     }
+    placeBombs() {
+        let emptyCells = this.getEmptyCells()
+        console.log(emptyCells)
+        // change these values to change range of possible bombs
+        let min = Math.floor(emptyCells.length * 0.30)
+        let max = Math.floor(emptyCells.length * 0.60)
+
+        let bombsToPlace = Math.floor(Math.random() * (max - min + 1)) + min
+        let bombsPlaced = 0
+        console.log("bombs to place",bombsToPlace)
+
+        // loop until all bombs are placed
+        while (bombsPlaced < bombsToPlace) {
+            const idx = Math.floor(Math.random() * emptyCells.length);
+            const cell = emptyCells.splice(idx, 1)[0]; // remove chosen cell from pool
+            cell.state = "bomb";
+            bombsPlaced++;
+        }
+
+        emptyCells = this.getEmptyCells()
+        console.log("markerCells: ",emptyCells)
+
+    }
+    identifyMarkers() {
+        let emptyCells = this.getEmptyCells()
+        console.log("markerCells: ",emptyCells)
+
+        emptyCells.forEach(cell => {
+            let surroundingCells = this.getAllSurroundingCells(cell)
+            let bombsTouchingCell = 0
+            surroundingCells.forEach(surroundingCell => {
+                if (surroundingCell.state == "bomb") {
+                    bombsTouchingCell++
+                }
+            })
+            if (bombsTouchingCell > 0) {
+                cell.state = "marker"
+                cell.marker = bombsTouchingCell
+            }
+        })
+    }
+    
 }
